1f656ad5e52232feff846d162b0fd0963c9b1cf8
Tasnim-Bahar-Samir <tanimsamir3@gmail.com>
2022-10-08 00:57:32 +0600

    completed order review page

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
+import './Order.css'
+import { Link, useLoaderData } from 'react-router-dom'
 import Cart from '../Cart/Cart';
 import OrderPreview from '../OrderPreview/OrderPreview';
+import { removeFromDb } from '../../utilities/fakedb';
 
 const Order = () => {
-  const {products,savedCart} = useLoaderData();
+  const {savedCart} = useLoaderData();
   const [savedProducts,setSavedProducts] = useState(savedCart);
+  const handleRemove = (id)=>{
+    const filteredProducts = savedProducts.filter(product => product.id !== id);
+    setSavedProducts(filteredProducts);
+    removeFromDb(id);
+  }
   return (
-    <div>
+    <div className='order-container'>
         <div className='shop-container'>
-          <div>
+          <div className='review-order-container'>
               {
-                savedProducts.map(product => <OrderPreview key={product.id} product = {product}/>)
+                savedProducts.map(product => <OrderPreview key={product.id} product = {product} onRemove ={handleRemove}/>)
+              }
+              {
+                savedProducts.length === 0 && <h2>No product ordered to review. <Link to='/'>Shop Now!</Link></h2>
               }
           </div>
           <div className="cart-details">
-            <Cart cart={savedCart}/>
+            <Cart cart={savedProducts}/>
           </div>
         </div>
     </div>
--- src/Components/OrderPreview/OrderPreview.js
+++ src/Components/OrderPreview/OrderPreview.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import './OrderPreview.css'
+import { BiTrash } from 'react-icons/bi';
 
-const OrderPreview = ({product}) => {
-    console.log(product)
-    // const{img}
+const OrderPreview = ({product,onRemove}) => {
+    // console.log(product)
+    const{id,img,name,quantity,price} = product;
   return (
-    <div>
+    <div className='order-review-container'>
         <div>
-           {/* <img src= alt="" />  */}
+           <img src={img} alt="" /> 
+        </div>
+        <div className='review-details-cont'>
+            <div>
+                <p>{name}</p>
+                <p className='small-details'>Price: <span>{price}</span></p>
+                <p className='small-details'>Quantity: <span>{quantity}</span></p>
+            </div>
+            <div>
+                <button onClick={()=>onRemove(id)} className='rmv-btn'>
+                <BiTrash className='trash'/>
+                </button>
+            </div>
         </div>
     </div>
   )
